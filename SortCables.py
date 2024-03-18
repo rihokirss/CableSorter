ea7459998a725f6d2e06f4257085140527bb8110
rihokirss <riho@kirss.ee>
2024-03-18 20:17:29 +0200

    dialoogiaknad olemas sisendi ja väljundi jaoks

--- SortCables.py
+++ SortCables.py
@@ -1,14 +1,18 @@
 import pandas as pd
 from collections import defaultdict
+from tkinter import Tk, filedialog
+from tkinter.simpledialog import askstring
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
 
 def correct_direction(row, direction_counter):
-    # Tagastab True, kui rea suund tuleks ümber pöörata
     if direction_counter[row['To']] > direction_counter[row['From']]:
         return True
     return False
 
 def sort_cables(input_file_path, sheet_name, output_file_path):
-    df = pd.read_excel(input_file_path, sheet_name=sheet_name)
+    # Lae andmed kasutades 'openpyxl', et säilitada formaat
+    df = pd.read_excel(input_file_path, sheet_name=sheet_name, engine='openpyxl')
     
     # Suuna tuvastamiseks kasutame sagedusloendurit
     direction_counter = defaultdict(int)
@@ -19,7 +23,7 @@ def sort_cables(input_file_path, sheet_name, output_file_path):
     # Kontrollime ja korrigeerime vajadusel suunda
     for index, row in df.iterrows():
         if correct_direction(row, direction_counter):
-            df.at[index, 'From'], df.at[index, 'To'] = row['To'], row['From']  # Vahetame suunda
+            df.at[index, 'From'], df.at[index, 'To'] = row['To'], row['From']
     
     graph = defaultdict(list)
     edges = set()
@@ -52,12 +56,39 @@ def sort_cables(input_file_path, sheet_name, output_file_path):
             if row is not None:
                 sorted_cables_df = pd.concat([sorted_cables_df, pd.DataFrame([row])], ignore_index=True)
     
-    with pd.ExcelWriter(output_file_path, engine='xlsxwriter') as writer:
+    # Salvestame tulpade laiused
+    book = load_workbook(input_file_path)
+    sheet = book[sheet_name]
+    column_widths = []
+    for col in sheet.columns:
+        max_length = 0
+        for cell in col:
+            try: # vajalik, et kontrollida tühje rakke
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        column_widths.append(max_length)
+    
+    # Salvestame sorteeritud andmed, kasutades 'openpyxl' mootorit
+    with pd.ExcelWriter(output_file_path, engine='openpyxl') as writer:
         sorted_cables_df.to_excel(writer, index=False)
- 
-# Määra failiteed
-input_file_path = 'C:\\Users\\riho\\Documents\\Coding\\CableSorter\\Vihik1.xlsx'
-output_file_path = 'C:\\Users\\riho\\Documents\\Coding\\CableSorter\\sorted_cables_correctly.xlsx'
- 
+        for i, width in enumerate(column_widths, start=1):
+            writer.sheets['Sheet1'].column_dimensions[get_column_letter(i)].width = width
+
+# Tkinter dialoogiakende jaoks
+root = Tk()
+root.withdraw() # Peidame Tkinteri põhiakna
+
+# Valime sisendfaili
+input_file_path = filedialog.askopenfilename(title="Vali sisendfail", filetypes=[("Excel files", "*.xlsx *.xls")])
+book = load_workbook(input_file_path, read_only=True)
+sheet_names = book.sheetnames
+sheet_name = askstring("Töölehe valik", "Sisesta töölehe nimi:\n" + "\n".join(sheet_names))
+
+# Valime väljundfaili asukoha
+output_file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
+
 # Kutsu funktsiooni välja
-sort_cables(input_file_path, 'Leht1', output_file_path)
+if input_file_path and sheet_name and output_file_path:
+    sort_cables(input_file_path, sheet_name, output_file_path)
