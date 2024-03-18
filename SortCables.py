52e15605e18856c4d53799c3df292787b89ec35a
rihokirss <84241469+rihokirss@users.noreply.github.com>
2024-03-18 22:45:45 +0200

    Update SortCables.py

--- SortCables.py
+++ SortCables.py
@@ -9,63 +9,57 @@ def correct_direction(row, direction_counter):
     # Kontrollib, kas "To" sagedus on suurem kui "From" sagedus
     return direction_counter[row['To']] > direction_counter[row['From']]
 
-# Peamine funktsioon kaablite sorteerimiseks
 def sort_cables(input_file_path, sheet_name, output_file_path):
-    # Laadib andmed DataFrame'i, säilitades algse vormingu
     df = pd.read_excel(input_file_path, sheet_name=sheet_name, engine='openpyxl')
     
-    # Loob sagedusloenduri suuna tuvastamiseks
     direction_counter = defaultdict(int)
     for _, row in df.iterrows():
         direction_counter[row['From']] += 1
         direction_counter[row['To']] += 1
 
-    # Korrigeerib kaablite suunda, kui vajalik
     for index, row in df.iterrows():
         if correct_direction(row, direction_counter):
             df.at[index, 'From'], df.at[index, 'To'] = row['To'], row['From']
 
-    # Loob graafi ja servade hulga kaablite marsruutide leidmiseks
     graph = defaultdict(list)
     edges = set()
     for _, row in df.iterrows():
         graph[row['From']].append(row['To'])
         edges.add((row['From'], row['To']))
 
-    # Leiab juured (alguspunktid)
     roots = set(graph.keys()) - {to for _, to in edges}
 
+    visited = set()
     sorted_cables = []
 
-    # Süvitsi otsingu (DFS) algoritm graafi läbimiseks
     def dfs(node, path=[]):
-        path.append(node)
-        for next_node in graph[node]:
-            if next_node not in path:
+        if node not in visited:
+            visited.add(node)
+            path.append(node)
+            for next_node in graph[node]:
                 dfs(next_node, path)
-        if not graph[node]:
-            sorted_cables.append(path.copy())
+            if not graph[node]:
+                sorted_cables.append(path.copy())
             path.pop()
+            visited.remove(node)
 
     for root in roots:
         dfs(root, [])
 
-    # Loob sorteeritud kaablite DataFrame'i
-    from_to_mapping = {(row['From'], row['To']): row for _, row in df.iterrows()}
-    sorted_cables_df = pd.DataFrame()
+    sorted_cables_df = pd.DataFrame(columns=df.columns)
     for path in sorted_cables:
         for i in range(len(path) - 1):
-            row = from_to_mapping.get((path[i], path[i+1]))
-            if row is not None:
-                sorted_cables_df = pd.concat([sorted_cables_df, pd.DataFrame([row])], ignore_index=True)
+            # Võtke esimene vastav rida
+            row = df[(df['From'] == path[i]) & (df['To'] == path[i+1])].iloc[0]
+            # Lisa see rida sorted_cables_df kasutades pd.concat
+            sorted_cables_df = pd.concat([sorted_cables_df, pd.DataFrame([row])], ignore_index=True)
 
-    # Salvestab sorteeritud andmed, säilitades veergude laiused
     book = load_workbook(input_file_path)
     sheet = book[sheet_name]
     column_widths = [max(len(str(cell.value)) for cell in col) for col in sheet.columns]
 
     with pd.ExcelWriter(output_file_path, engine='openpyxl') as writer:
-        sorted_cables_df.to_excel(writer, index=False)
+        sorted_cables_df.to_excel(writer, sheet_name='Sheet1', index=False)
         for i, width in enumerate(column_widths, start=1):
             writer.sheets['Sheet1'].column_dimensions[get_column_letter(i)].width = width
 
